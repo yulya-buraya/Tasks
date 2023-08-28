3758cbb09133b472067d41276c01b9ef2cd8482c
buraya.yuliya <burayayulya374@gmail.com>
2023-08-28 16:43:54 +0300

    solved the problem on the composition function

---
+++ compose/composeFunction.js
@@ -0,0 +1,5 @@
+//Compose function (x -number, fns - array function)
+const compose = (...fns) => (x) => fns.reduceRight((res, fn) => fn(res), x);
+//Compose function (x -array number, fns - array function)
+const composeWithArgs =  (...fns) => fns.reduceRight((res, fn) => (...arg) =>fn(res(...arg)));
+
